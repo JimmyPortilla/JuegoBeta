a54db823baac34ef8d2924f918687165d8d952ac
jimmy portilla <japortilla1810@gmail.com>
2020-06-09 21:31:03 -0500

    Diferentes html

--- src/main/resources/static/js/js.js
+++ src/main/resources/static/js/js.js
@@ -1,8 +1,11 @@
 
 var tabla_carreras;
+var tabla_personas;
+var tabla_materias;
 
 const url_personas= 'https://spring-boot-juegobeta.herokuapp.com/getPerson';
-const url_carreras= 'https://spring-boot-juegobeta.herokuapp.com/getCarrera'
+const url_carreras= 'https://spring-boot-juegobeta.herokuapp.com/getCarrera';
+const url_materias= 'https://spring-boot-juegobeta.herokuapp.com/getMateria';
 
 
 function cargar_carreras(){
@@ -11,11 +14,11 @@ function cargar_carreras(){
     .then((datos) => {
        console.log(datos);
        cont =0;
-       tabla_materias.innerHTML='';
+       tabla_carrera.innerHTML='';
 		for(let valor of datos){
 			console.log(valor.nombre);
 			cont = cont+1;
-            tabla_materias.innerHTML += `
+            tabla_carrera.innerHTML += `
             <tr>
                 <th scope="row">`+ cont +` </th>
                 <td>${valor.idArea}</td>
@@ -66,9 +69,44 @@ function cargar_personas(){
 
 
 
+function cargar_materias(){
+	fetch(url_materias)
+    .then(res => res.json())
+    .then((datos) => {
+       console.log(datos);
+       cont =0;
+       tabla_materias.innerHTML='';
+		for(let valor of datos){
+			console.log(valor.nombre);
+			cont = cont+1;
+            tabla_materias.innerHTML += `
+            <tr>
+                <th scope="row">`+ cont +` </th>
+                <td>${valor.idCarrera}</td>
+                <td>${valor.codigoMateria}</td>
+                <td>${valor.nombre}</td>
+                <td><button class="btn btn-warning" onclick="editarUsuario('${valor.idCarrera}', '${valor.codigoMateria}', '${valor.nombre}')" >Editar</button></td>
+                <td>
+                <form action="/eliminarUsuario" method="POST">
+                    <input type="text" name="id" class="form-control" value="${valor.codigoMateria}" style="display: none;">
+                    <button type="submit" class="btn btn-danger">Eliminar</button>
+                </form>
+                </td>
+            </tr>
+            `
+		}
+    }).catch(err => console.error(err));
+}
+
+
+
 
 $(document).ready(function(){
     alert('jajaja');
     tabla_carreras = document.getElementById("tabla_carrera");
     cargar_carreras();
+    tabla_personas = document.getElementById("tabla_persona");
+    cargar_personas();
+    tabla_materias = document.getElementById("tabla_materia");
+    cargar_materias();
 });
